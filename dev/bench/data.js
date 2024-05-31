919c07f852935dd8950293f62f8bc55a78d1a349
github-action-benchmark <github@users.noreply.github.com>
2024-05-31 05:37:17 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for f5c2ac686f5f5c75b29ab89fed9d735743dfe6ac

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717126433550,
+  "lastUpdate": 1717133836998,
   "repoUrl": "https://github.com/commetchat/commet",
   "entries": {
     "Benchmark": [
@@ -44,6 +44,51 @@ window.BENCHMARK_DATA = {
           {
             "name": "TimelineViewer Scrolling - 95th Percentile Build Time",
             "value": 3.625,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "commetchat",
+            "username": "commetchat"
+          },
+          "committer": {
+            "name": "commetchat",
+            "username": "commetchat"
+          },
+          "id": "f5c2ac686f5f5c75b29ab89fed9d735743dfe6ac",
+          "message": "Create benchmarks",
+          "timestamp": "2024-05-30T07:32:59Z",
+          "url": "https://github.com/commetchat/commet/pull/255/commits/f5c2ac686f5f5c75b29ab89fed9d735743dfe6ac"
+        },
+        "date": 1717133836764,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "TimelineViewer Scrolling - Average Build Time",
+            "value": 1.967744680851063,
+            "unit": "ms"
+          },
+          {
+            "name": "TimelineViewer Scrolling - Average Raster Time",
+            "value": 0.9034785714285714,
+            "unit": "ms"
+          },
+          {
+            "name": "TimelineViewer Scrolling - Worst Build Time",
+            "value": 5.95,
+            "unit": "ms"
+          },
+          {
+            "name": "TimelineViewer Scrolling - Worst Raster Time",
+            "value": 2.967,
+            "unit": "ms"
+          },
+          {
+            "name": "TimelineViewer Scrolling - 95th Percentile Build Time",
+            "value": 4.206,
             "unit": "ms"
           }
         ]
