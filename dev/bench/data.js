0ec56da9021342d9fba11b2bf6493b222ddf953e
github-action-benchmark <github@users.noreply.github.com>
2024-05-31 03:33:53 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 87a3705c3bdd67d815dc68c29d88f992e9252f0b

---
+++ dev/bench/data.js
@@ -0,0 +1,53 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1717126433550,
+  "repoUrl": "https://github.com/commetchat/commet",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "commetchat",
+            "username": "commetchat"
+          },
+          "committer": {
+            "name": "commetchat",
+            "username": "commetchat"
+          },
+          "id": "87a3705c3bdd67d815dc68c29d88f992e9252f0b",
+          "message": "Create benchmarks",
+          "timestamp": "2024-05-30T07:32:59Z",
+          "url": "https://github.com/commetchat/commet/pull/255/commits/87a3705c3bdd67d815dc68c29d88f992e9252f0b"
+        },
+        "date": 1717126433281,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "TimelineViewer Scrolling - Average Build Time",
+            "value": 1.7420296296296296,
+            "unit": "ms"
+          },
+          {
+            "name": "TimelineViewer Scrolling - Average Raster Time",
+            "value": 1.0757537313432832,
+            "unit": "ms"
+          },
+          {
+            "name": "TimelineViewer Scrolling - Worst Build Time",
+            "value": 4.165,
+            "unit": "ms"
+          },
+          {
+            "name": "TimelineViewer Scrolling - Worst Raster Time",
+            "value": 4.209,
+            "unit": "ms"
+          },
+          {
+            "name": "TimelineViewer Scrolling - 95th Percentile Build Time",
+            "value": 3.625,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
