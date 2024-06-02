c50d407a6959c08657064686d19409bcd9519065
github-action-benchmark <github@users.noreply.github.com>
2024-06-02 09:50:06 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for f545bb7e0951d75731f672e9c2334493f5400b4f

---
+++ dev/bench/data.js
@@ -0,0 +1,73 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1717321806715,
+  "repoUrl": "https://github.com/commetchat/commet",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "commetchat",
+            "username": "commetchat"
+          },
+          "committer": {
+            "name": "commetchat",
+            "username": "commetchat"
+          },
+          "id": "f545bb7e0951d75731f672e9c2334493f5400b4f",
+          "message": "Create benchmarks",
+          "timestamp": "2024-06-02T01:10:29Z",
+          "url": "https://github.com/commetchat/commet/pull/255/commits/f545bb7e0951d75731f672e9c2334493f5400b4f"
+        },
+        "date": 1717321806435,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "TimelineViewer Scrolling - Timeline Event Build Count",
+            "value": 25235,
+            "unit": "Builds"
+          },
+          {
+            "name": "TimelineViewer Scrolling - Average Build Time",
+            "value": 2.3670724637681158,
+            "unit": "ms"
+          },
+          {
+            "name": "TimelineViewer Scrolling - Average Raster Time",
+            "value": 1.6107971014492752,
+            "unit": "ms"
+          },
+          {
+            "name": "TimelineViewer Scrolling - Worst Build Time",
+            "value": 5.677,
+            "unit": "ms"
+          },
+          {
+            "name": "TimelineViewer Scrolling - Worst Raster Time",
+            "value": 3.827,
+            "unit": "ms"
+          },
+          {
+            "name": "TimelineViewer Scrolling - 99th Percentile Build Time",
+            "value": 5.594,
+            "unit": "ms"
+          },
+          {
+            "name": "TimelineViewer Scrolling - 90th Percentile Build Time",
+            "value": 5.594,
+            "unit": "ms"
+          },
+          {
+            "name": "TimelineViewer Scrolling - 99th Percentile Raster Time",
+            "value": 3.467,
+            "unit": "ms"
+          },
+          {
+            "name": "TimelineViewer Scrolling - 90th Percentile Raster Time",
+            "value": 2.266,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
